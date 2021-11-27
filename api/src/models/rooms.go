f7e7cdc63f2638e818e0a5d213f81a3e18322475
sakuyasann <sakuya@sakuyasan.net>
2021-11-27 19:01:57 +0900

    JSONをキャメルケースに変換

--- api/src/models/rooms.go
+++ api/src/models/rooms.go
@@ -2,8 +2,8 @@ package models
 
 type Rooms struct {
 	Model
-	RoomNumber int    `json:"room_number"`
-	RoomStatus string `json:"room_status" gorm:"type: enum('open', 'close'); default:'open'; not null"`
+	RoomNumber int    `json:"roomNumber"`
+	RoomStatus string `json:"roomStatus" gorm:"type: enum('open', 'close'); default:'open'; not null"`
 	UsersID    uint   `json:"-"`
 	OpponentId uint   `json:"-"`
 }
